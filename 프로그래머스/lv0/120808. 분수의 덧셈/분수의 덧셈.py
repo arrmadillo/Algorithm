a0456de6ff6788e0b35da4f51998c372556bc150
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-03 16:59:30 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,7 +1,4 @@
-import fractions
+from fractions import Fraction
 def solution(denum1, num1, denum2, num2):
-    a = fractions.Fraction(denum1, num1)
-    b = fractions.Fraction(denum2, num2)
-    c = a + b
-    
-    return [c.numerator, c.denominator]
+    answer = Fraction(denum1, num1) + Fraction(denum2, num2)
+    return [answer.numerator, answer.denominator]
