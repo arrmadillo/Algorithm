213d0de125fbf7ce7a7a60ae6d5651c177883267
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-17 16:32:31 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -0,0 +1,25 @@
+import sys
+int_num = int(sys.stdin.readline())
+str_num = str(int_num)
+add = 0
+count = 0
+
+def answer(str_num, count):
+    while True:
+        add = int(str_num[0]) + int(str_num[1])
+        add = str(add)
+        str_num = str_num[-1] + add[-1]
+        count += 1
+        if str(int_num) == str_num:
+            print(count)
+            break
+
+if int_num == 0:
+    print(1)
+elif int_num < 10:
+    str_num += '0'
+    int_num = int(str_num)
+    answer(str_num, count)
+    
+else:
+    answer(str_num, count)
