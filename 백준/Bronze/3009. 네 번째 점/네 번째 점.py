70ec1569e6830007ab49db407baf7827eb2d1437
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-17 16:49:34 +0900

    [Bronze III] Title: 네 번째 점, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,13 +1,8 @@
-nums1 = list(map(int, input().split()))
-nums2 = list(map(int, input().split()))
-nums3 = list(map(int, input().split()))
-sum_nums = [nums1, nums2, nums3]
+x = []
+y = []
+for i in range(3):
+    a, b = list(map(int, input().split()))
+    x.append(a)
+    y.append(b)
 
-index0 = 0
-index1 = 0
-temp0 = []
-temp1 = []
-for nums in sum_nums:
-    temp0.append(nums[0])
-    temp1.append(nums[1])
-print(min(temp0, key=temp0.count), min(temp1, key=temp1.count))
+print(min(x, key=x.count), min(y, key=y.count))
