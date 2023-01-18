f3ed38ffcbe22b71df84fdb5d5df92c8d91f017d
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-18 16:55:28 +0900

    [Bronze II] Title: 태보태보 총난타, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/17249. 태보태보 총난타/태보태보 총난타.py
+++ 백준/Bronze/17249. 태보태보 총난타/태보태보 총난타.py
@@ -2,16 +2,5 @@ import sys
 
 punch = sys.stdin.readline().rstrip()
 punch = punch.split("(^0^)")
-left = 0
-right = 0
 
-for i in punch[0]:
-    if i == '@':
-        left += 1
-for i in punch[1]:
-    if i == '@':
-        right += 1
-
-print(left, right)
-
-
+print(punch[0].count('@'), punch[1].count('@'))
