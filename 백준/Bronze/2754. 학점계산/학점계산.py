6f1813cf9730fded53aaa0bd1e850e1bc5fdccab
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-19 14:02:54 +0900

    [Bronze V] Title: 학점계산, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.py
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -1,10 +1,8 @@
 import sys
 
-nums = sys.stdin.readline().rstrip()
-grade = {'A+': 4.3, 'A0': 4.0, 'A-': 3.7,
-        'B+': 3.3, 'B0': 3.0, 'B-': 2.7,
-        'C+': 2.3, 'C0': 2.0, 'C-': 1.7,
-        'D+': 1.3, 'D0': 1.0, 'D-': 0.7,
-        'F' : 0.0}
-print(grade[nums])
+text = sys.stdin.readline().rstrip()
+
+grade = {'A':4, 'B':3, 'C':2, 'D':1, '+':0.3, '0':0.0, '-':-0.3}
+
+print(0.0 if text == 'F' else grade[text[0]] + grade[text[1]])
 
