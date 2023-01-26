a7c2311463aed43c639f58a7217c5aecbdde35f5
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-26 09:28:30 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -3,13 +3,11 @@ import sys
 status = 1
 
 while status == 1:
-    try:
         t = sys.stdin.readline().rstrip()
         if t == '':
             status = 0
         print(t)
-    except:
-        status = 0
+
 
 
 
