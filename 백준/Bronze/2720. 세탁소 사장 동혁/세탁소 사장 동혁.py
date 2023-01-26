c84b153f0edf4c5164babf60c2dc26973244aa0c
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-26 16:07:32 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -2,24 +2,12 @@ import sys
 
 t = int(sys.stdin.readline())
 answer = [0, 0, 0, 0]
-
+money = [25, 10, 5, 1]
 for i in range(t):
     change = int(sys.stdin.readline())
     while change != 0:
-        if change >= 25:
-            answer[0] = change // 25
-            change = change - (answer[0] * 25)
-        elif change >= 10:
-            answer[1] = change // 10
-            change = change - (answer[1] * 10)
-        elif change >= 5:
-            answer[2] = change // 5
-            change = change - (answer[2] * 5)
-        else:
-            answer[3] = change // 1
-            change = change - (answer[3] * 1)
+        for i in range(4):
+            answer[i] = change // money[i]
+            change = change - (answer[i] * money[i])
     print(*answer)
     answer = [0, 0, 0, 0]
-
-            
-
