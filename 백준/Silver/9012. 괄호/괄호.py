2087a06019e0779faf64c30f09a1dc1ce4a1a8c6
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-26 14:49:50 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,25 @@
+import sys
+
+t = int(sys.stdin.readline())
+a, b = 0, 0
+for i in range(t):
+    vps = sys.stdin.readline().rstrip()
+    if len(vps) % 2 != 0:
+        print('NO')
+    elif vps.count('(') != vps.count(')'):
+        print('NO')
+    else:
+        for i in vps:
+            if a >= b:
+                if i == '(':
+                    a += 1
+                else:
+                    b += 1
+            else:
+                print('NO')
+                a, b = 0, 0
+                break
+        else:
+            print('YES')
+            
+
