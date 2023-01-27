6b71416cb6a773b09cf9618567fb9fad801b15a7
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-27 14:39:32 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,13 +1,12 @@
 import sys
 
-nums = [int(sys.stdin.readline()) % 42 for i in range(10)]
-result = []
-for i in nums:
-    if i not in result:
-        result.append(i)
-print(len(result))
+nums = []
 
+for i in range(10):
+    num = int(sys.stdin.readline()) % 42
+    nums.append(num)
 
+print(len(set(nums))) 
 
 
 
