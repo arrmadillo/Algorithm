5bec16d98b69ac5784fad59434911688ca15af34
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-27 14:26:00 +0900

    [Silver IV] Title: 대칭 차집합, Time: 224 ms, Memory: 87452 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -2,12 +2,10 @@ import sys
 
 case = sys.stdin.readline().rstrip()
 
-nums1 = set(map(int, sys.stdin.readline().split()))
-nums2 = set(map(int, sys.stdin.readline().split()))
+nums1 = set(sys.stdin.readline().split())
+nums2 = set(sys.stdin.readline().split())
 
-answer = 0
-answer += len(nums1-nums2) + len(nums2 - nums1) 
-print(answer)
+print(len(nums1-nums2) + len(nums2 - nums1) )
 
 
 
