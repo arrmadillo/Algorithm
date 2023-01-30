a0a08aa14ef3b4d047067a4a394d703583f30f10
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-30 16:32:40 +0900

    [Bronze III] Title: 나는 요리사다, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
@@ -3,7 +3,7 @@ import sys
 result = []
 
 for i in range(1, 6):
-    t = sum(list(map(int, sys.stdin.readline().split())))
+    t = sum(map(int, sys.stdin.readline().split()))
     result.append((t,i))
 
 result = sorted(result, reverse=True)
