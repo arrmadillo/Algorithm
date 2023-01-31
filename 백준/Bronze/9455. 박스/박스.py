6a840ea26dcb75089f798d9aec4a460a2ceae4d4
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-01-31 23:37:30 +0900

    [Bronze I] Title: 박스, Time: 120 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9455. 박스/박스.py
@@ -0,0 +1,24 @@
+import sys
+
+case = int(sys.stdin.readline().rstrip())
+
+for _ in range(case): #케이스
+    lines, num = map(int, sys.stdin.readline().rstrip().split())# 줄, 개수
+    #temp = []
+    temp = [list(map(int, sys.stdin.readline().rstrip().split())) for i in range(lines)]
+    answer = 0
+
+    # for _ in range(lines):
+    #     temp.append(list(map(int, sys.stdin.readline().rstrip().split())))
+    
+    for col in range(num):
+        count = 0 #움직여야할 거리
+        for row in range(lines-1, -1, -1):
+            if temp[row][col] == 1:
+                    answer += count
+            else:
+                count +=1
+    print(answer) 
+
+
+
