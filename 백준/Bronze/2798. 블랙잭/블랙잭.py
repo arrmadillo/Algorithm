9bdde02ac8b2daa9e88f6cf6500761281b5c0835
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-02-01 13:09:35 +0900

    [Bronze II] Title: 블랙잭, Time: 92 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -3,13 +3,12 @@ n, m = map(int, input().split())
 card = list(map(int, input().split()))
 max_num = 0
 
-for num1 in range(n-2):
-    for num2 in range(num1+1, n-1):
+for num1 in range(n):
+    for num2 in range(num1+1, n):
         for num3 in range(num2+1, n):
             if card[num1] + card[num2] + card[num3] > m:
                 continue
             elif card[num1] + card[num2] + card[num3] > max_num:
                 max_num = card[num1] + card[num2] + card[num3]
-                
-
+        
 print(max_num)
