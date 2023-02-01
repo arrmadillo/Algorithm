2be33de21fa4247031249c5926afc5c109007e98
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-02-01 16:40:45 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 292 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -1,9 +1,10 @@
 import sys
 
-num = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 
-while True:
+for num in range(n, 3, -1):
     if len(str(num)) == str(num).count('4') + str(num).count('7'):
         print(num)
         break
-    num -= 1
+
+
