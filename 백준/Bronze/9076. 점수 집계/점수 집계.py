d6a4c4e67d72d6c924b22895719afd1240c58fe9
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-02-01 14:21:07 +0900

    [Bronze II] Title: 점수 집계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9076. 점수 집계/점수 집계.py
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -4,25 +4,16 @@ t = int(sys.stdin.readline())
 
 for _ in range(t):
     score = list(map(int, sys.stdin.readline().split())) 
-    new_score = score[:]
-    max_num = max(new_score)
-    min_num = min(new_score)
-    new_score.remove(max_num)
-    new_score.remove(min_num)
-
-    new_score = sorted(new_score)
+    score = sorted(score)
+    max_num = score[-1]
+    min_num = score[0]
+    new_score = score[1:-1]
+    
     if new_score[-1] - new_score[0] >= 4:
         print('KIN')
     else:
         print(sum(new_score))
-    # for score in new_score:
-    #     print('점수:',score)
-    #     if max_num - score >= 4 or score - min_num > 4:
-    #         print('KIN')
-    #         # print(max_num)
-    #         break
-    # else:
-    #     print(sum(new_score))
+
 
 
 
