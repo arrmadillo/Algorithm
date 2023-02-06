6ea324fd86d457e039ac990687c0e54b4dc1c9d3
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-02-06 16:39:06 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -2,19 +2,18 @@ import sys
 
 
 dot = int(sys.stdin.readline()) #컴퓨터 개수
-line = int(sys.stdin.readline()) #연결 선
+lines = int(sys.stdin.readline()) #연결 선
 
+graph = [[] for _ in range(dot + 1)] # n번 컴퓨터와 연결된 컴퓨터들 
+visited = [False] * (dot + 1)
 
-graph = [[] for _ in range(dot+1)]
-for i in range(line):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-    
-visited = [False] * len(graph)
-visited[1] = True #1부터 시작이니깐
+for _ in range(lines):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
 stack = [1]
-answer = []
+visited[1] = True
 
 while stack:
     cur = stack.pop()
@@ -23,6 +22,5 @@ while stack:
         if not visited[num]:
             visited[num] = True
             stack.append(num)
-            answer.append(num)
 
-print(len(answer))
+print(visited.count(True)-1) #컴퓨터1은 제외 (1 때문에 감염된 바이러스 컴퓨터의 수를 구하기 위해)    
